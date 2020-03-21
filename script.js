c0a46d3cedbb34c1d75555d02fdca17ddea3a795
Serhii Chernysh <srg.chernysh@gmail.com>
2020-03-21 20:50:31 +0200

    feat: implement sticky navbar

--- script.js
+++ script.js
@@ -3,19 +3,36 @@ const NAV = document.querySelector('.main-nav');
 const PORTFOLIO_LIST = document.querySelector('.portfolio__list');
 const PROJECT_FORM = document.querySelector('.project-form');
 const SUBMIT = document.querySelector('.project-form__btn');
-const USER_NAME = document.getElementById('user-name');
-const USER_EMAIL = document.getElementById('user-email');
 const MODAL = document.querySelector('.modal');
 const MODAL_SUBJECT = document.querySelector('.modal__subject');
 const MODAL_DESCRIPTION = document.querySelector('.modal__description');
 const MODAL_BTN = document.querySelector('.modal__btn');
 const OVERLAY = document.querySelector('.overlay');
+const USER_NAME = document.getElementById('user-name');
+const USER_EMAIL = document.getElementById('user-email');
 
-NAV.addEventListener('click', (event) => {
-  let target = event.target;
-  if (target.tagName !== 'A') return;
-  NAV.querySelectorAll('.main-nav__link').forEach(el => el.classList.remove('main-nav__link--active'));
-  target.classList.add('main-nav__link--active');
+// NAV.addEventListener('click', (event) => {
+//   let target = event.target;
+//   if (target.tagName !== 'A') return;
+//   NAV.querySelectorAll('.main-nav__link').forEach(el => el.classList.remove('main-nav__link--active'));
+//   target.classList.add('main-nav__link--active');
+// });
+
+document.addEventListener('scroll', (event) => {
+  const curPosY = window.pageYOffset;
+  const anchors = document.querySelectorAll('a.anchor');
+  const links = document.querySelectorAll('.main-nav__link');
+  
+  anchors.forEach((el) => {
+    if (el.offsetTop <= curPosY && (el.offsetTop + el.parentElement.offsetHeight) > curPosY) {
+      links.forEach((a) => {
+        a.classList.remove('main-nav__link--active');
+        if (el.getAttribute('id') === a.getAttribute('href').slice(1)) {
+          a.classList.add('main-nav__link--active');
+        }
+      })
+    }
+  });
 });
 
 PORTFOLIO_LIST.addEventListener('click', (event) => {
